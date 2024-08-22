3108807cc518ba8b1ca72c9d52aa3d4b26197e7b
Yura Sarvas <grey-matter88@mail.ru>
2024-08-22 22:09:26 +0300

    Добавление моделей каталога и продуктов+ миграция и загрузка в базу данных

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from catalog.apps import CatalogConfig
 from catalog.views import home, contacts
 
 app_name = CatalogConfig.name
 urlpatterns = [
-    path('', home, name='home'),
-    path('contacts/', contacts, name='contacts')
-]
+    path("", home, name="home"),
+    path("contacts/", contacts, name="contacts"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
